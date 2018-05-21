aead61895510d431a454080878a73910647ee82d
kavi <ogs.kavi@gmail.com>
2018-05-21 22:43:25 +0530

    adding latest content

---
+++ data/users.js
@@ -0,0 +1,98 @@
+var users = ({
+    arnab: {
+      login: 'arnab',
+      name: 'Arnab Datta'
+    },
+    ashokv: {
+      login: 'ashokv',
+      name: 'Ashok Venkatramani'
+    },
+    bix: {
+      login: 'bix',
+      name: 'Sanjeev Bikhchandani'
+    },
+    chaks: {
+      login: 'chaks',
+      name: 'Parthasarathy Chakrapani'
+    },
+    goki: {
+      login: 'goki',
+      name: 'Ranjit Gokarn'
+    },
+    JBajaj: {
+      login: 'JBajaj',
+      name: 'Jagdish Bajaj'
+    },
+    katyal: {
+      login: 'katyal',
+      name: 'Sanjay Katyal'
+    },
+    kavi: {
+      login: 'kavi',
+      name: 'Kaviraj Singh'
+    },
+    kvs: {
+      login: 'kvs',
+      name: 'KVS'
+    },
+    mln: {
+      login: 'mln',
+      name: 'MLN Prasad Mangipudi'
+    },
+    msramesh: {
+      login: 'msramesh',
+      name: 'M S Ramesh'
+    },
+    nikesh: {
+      login: 'nikesh',
+      name: 'Nikesh Sinha'
+    },
+    prof: {
+      login: 'prof',
+      name: 'Arvind Sahay'
+    },
+    rajesh: {
+      login: 'rajesh',
+      name: 'Rajesh Singh'
+    },
+    ranveer: {
+      login: 'ranveer',
+      name: 'Ranveer'
+    },
+    nix: {
+      login: 'nix',
+      name: 'Sunil Nikhar'
+    },
+    vasu: {
+      login: 'vasu',
+      name: 'Vasu'
+    },
+    vinaykumar: {
+      login: 'vinaykumar',
+      name: 'Vinay Kumar'
+    },
+    vmk: {
+      login: 'vmk',
+      name: 'VMK'
+    },
+    sanjeev: {
+      login: 'sanjeev',
+      name: 'Sanjeev'
+    },
+    easundaram: {
+      login: 'easundaram',
+      name: 'EA Sundaram'
+    },
+    shumone: {
+      login: 'shumone',
+      name: 'Shumone'
+    },
+    siddarth: {
+      login: 'siddarth',
+      name: 'Siddarth'
+    }
+
+})
+
+exports = module.exports = users;
+
