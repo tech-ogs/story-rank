07d3837d02c220f917af86db469fe8e05f8456a5
kavi <ogs.kavi@gmail.com>
2018-05-22 07:46:55 +0530

    content patches

---
+++ server/sql/patches/patch2.sql
@@ -0,0 +1,3 @@
+update application.stories set attributes =  jsonb_set(attributes, '{image}', '"assets/thumbs/punjabi.jpg"') where id = 56;
+update application.stories set attributes =  jsonb_set(attributes, '{image}', '"assets/thumbs/vishnu-avatar-kalki.jpg"') where id = 55;
+update application.stories set attributes =  jsonb_set(attributes, '{url}', '"assets/forum-chat-on-religion.txt"')  where id = 35;
