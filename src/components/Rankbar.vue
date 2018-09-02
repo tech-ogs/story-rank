a1ce77ef772412da93ee062f00dbb6e943b619f6
root <kavi@saralweb.com>
2018-09-01 23:33:10 -0700

    Adding missing files

---
+++ src/components/Rankbar.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <b-navbar class="rank-nav-bar" >
+        Your top 3<br>
+        <b-nav pills fill class="width100 rank-buttons-bar">
+           <rank-button :pos=0  @rank-button-click="handleRankBtnClick" > </rank-button>
+           <rank-button :pos=1  @rank-button-click="handleRankBtnClick"> </rank-button>
+           <rank-button :pos=2  @rank-button-click="handleRankBtnClick"> </rank-button>
+        </b-nav>
+    </b-navbar>
+
+</template>
+
+<script>
+const fields = [
+  {
+    key: 'content',
+    label: '',
+    sortable: false
+  }
+]
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    items() { 
+		return this.$store.getters.storiesGetAllResults
+    },
+    myranks() { return this.$store.getters.myranks},
+    selectedRow() { return this.$store.getters.dashSelectedRow},
+  },
+  methods: {
+		getImg: (url) => { return require('@/'+url) },
+		handleRankBtnClick (val)  {
+			this.$emit('rank-button-click', val)
+		}
+  },
+  watch: {
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.rank-nav-bar {
+	width: 100%;
+}
+.rank-buttons-bar {
+}
+
+</style>
---
+++ src/components/Rankbutton.vue
@@ -0,0 +1,75 @@
+<template>
+    <b-nav-item class=""  @click="handleClick">
+		<div v-if="itemsById[myranks[pos]] != null">
+		<b-img  
+			thumbnail rounded fluid rounded="circle" 
+			class="rank-button-image" 
+			:src="getImg(itemsById[myranks[pos]].attributes.image)"
+		> 
+		</b-img>
+    	<br>
+    	{{ itemsById[myranks[pos]].name.substr(0,10) }}
+		</div>
+		<div v-else class="">
+			<div class="rank-button-placeholder flex-perfect-center"> {{ pos + 1 }} 
+			</div>
+		</div>
+    </b-nav-item>
+
+</template>
+
+<script>
+const fields = [
+  {
+    key: 'content',
+    label: '',
+    sortable: false
+  }
+]
+
+export default {
+  data () {
+    return {
+    }
+  },
+  props: ['pos'],
+  computed: {
+    itemsById() { 
+		return this.$store.getters.storiesGetIdMap
+    },
+    myranks () {
+        return this.$store.getters.myranks
+    }
+  },
+  methods: {
+      getImg: (url) => { return require('@/'+url) },
+      handleClick: function () { 
+		this.$emit('rank-button-click', this.pos)
+      }
+  },
+
+  mounted () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .rank-button-image{
+        display: inline-flex;
+        align: top;
+        height: 80px;
+        width: 80px;            
+    }
+	.rank-button-placeholder{
+		font-size: x-large;
+        display: inline-flex;
+        align: top;
+        height: 80px;
+        width: 80px;
+		-moz-border-radius: 40px; 
+		-webkit-border-radius: 40px; 
+		border-radius: 40px;
+		background-color: gold;
+	}
+</style>
